32322d0af8e2e6c75c8d41e7852d7736e7d535e4
Philip Sole <webdesign@philipsole.com>
2024-12-14 12:43:12 +1300

    pretty finished collection swatches

--- assets/collection-swatches.js
+++ assets/collection-swatches.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log('collection swatches');
+
+  document.addEventListener('click', (e) => {
+    const swatchWrapper = e.target.closest('[data-swatch-image]');
+    if(swatchWrapper) {
+        const imageUrl = swatchWrapper.getAttribute('data-swatch-image');
+
+        let imageToSwap = swatchWrapper.closest('.card').querySelector('.media > img');
+        imageToSwap.removeAttribute('srcset');
+        imageToSwap.src = imageUrl;
+    }
+
+  });
 });
